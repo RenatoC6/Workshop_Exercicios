f2b4bffbd9839e29b9bb9ac708ebf8cd0ccbab6f
Renato Campion <campionr6@gmail.com>
2025-06-24 20:04:54 -0300

    PessoasStream

---
+++ src/Exercicios_conceitos_java/PessoasStream.java
@@ -0,0 +1,35 @@
+package Exercicios_conceitos_java;
+
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
+
+public class PessoasStream {
+
+    public static void main(String[] args) {
+
+        Map<String, Integer> pessoas = new HashMap<>();
+        pessoas.put("Maria", 20);
+        pessoas.put("Joao", 31);
+        pessoas.put("Pedro", 50);
+        pessoas.put("Antonio", 29);
+
+        Set<String> maisDe30 = new HashSet<>();
+
+        for (String nome : pessoas.keySet()) {
+            int idade = pessoas.get(nome);
+            if (idade > 30) {
+                maisDe30.add(nome);
+            }
+        }
+
+        System.out.println("Pessoas com mais de 30 anos:");
+        for (String nome : maisDe30) {
+            System.out.println(nome);
+        }
+
+
+    }
+
+}
