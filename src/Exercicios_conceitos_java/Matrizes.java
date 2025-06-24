5cf8cf1363d9a10575378744348121940b33e717
Renato Campion <campionr6@gmail.com>
2025-06-24 18:11:10 -0300

    matrizes

---
+++ src/Exercicios_conceitos_java/Matrizes.java
@@ -0,0 +1,22 @@
+package Exercicios_conceitos_java;
+
+public class Matrizes {
+
+    public static void main(String[] args) {
+
+        int [][] matriz = {{1,2,3},{4,5,6},{7,8,9}};
+
+        int soma =0;
+
+        for(int linha = 0; linha < matriz.length; linha++){
+            int somaLinha = 0;
+            for(int coluna = 0; coluna < matriz[0].length; coluna++){
+                System.out.print( matriz[linha][coluna] + " ");
+                somaLinha += matriz[linha][coluna];
+                soma += matriz[linha][coluna];
+            }
+            System.out.println("= soma da linha: " + somaLinha);
+        }
+        System.out.println("soma total da matriz: " + soma);
+    }
+}
