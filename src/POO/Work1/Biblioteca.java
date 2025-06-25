b56ae8e9dd6c9c174b871ce4f52cce2309b45676
Renato Campion <campionr6@gmail.com>
2025-06-25 17:11:56 -0300

    POO Work1

---
+++ src/POO/Work1/Biblioteca.java
@@ -0,0 +1,77 @@
+package POO.Work1;
+
+import java.util.ArrayList;
+
+public class Biblioteca {
+
+    ArrayList<Livro> livros = new ArrayList<>();
+
+    public void adicionarLivro(Livro livro) {
+        livros.add(livro);
+        System.out.println("Livro adicionado: " + livro); // o metodo "toString" na classe Livro mostra
+        // o livro adicionado de forma legivel
+    }
+
+    public void listarLivros(){
+        if (livros.isEmpty()) {
+            System.out.println("Nenhum livro na biblioteca.");
+        } else {
+            System.out.println();
+            System.out.println("Livros na biblioteca:");
+            for (Livro livro : livros) { // for each loop - percorre a array livros que contem objetos tipo Livro
+                System.out.println("Livro: " + livro.getTitulo());
+            }
+        }
+
+    }
+
+    public boolean buscaLivros(String titulo) {
+        System.out.println();
+        System.out.println("Buscando livros por titulo: ");
+        String retornoTitulo= titulo;
+        for (Livro livro : livros) {
+            if (livro.getTitulo().equalsIgnoreCase(titulo)) {
+                retornoTitulo =  livro.getTitulo();
+                return true;
+            }
+
+        }
+        return false;
+    }
+
+
+    public static void main(String[] args) {
+        Biblioteca biblioteca = new Biblioteca();
+
+        Livro livro1 = new Livro();
+        livro1.setTitulo("Livro 1");
+        livro1.setAutor("joao");
+        livro1.setAnoPublicacao(2020);
+
+        biblioteca.adicionarLivro(livro1);
+
+        Livro livro2 = new Livro();
+        livro2.setTitulo("Livro 2");
+        livro2.setAutor("Maria");
+        livro2.setAnoPublicacao(2025);
+
+        biblioteca.adicionarLivro(livro2);
+
+        biblioteca.listarLivros();
+
+        //String titulo = "livro 1";
+        String titulo = "xxxxx";
+
+        boolean achou  = biblioteca.buscaLivros(titulo);
+        if (achou) {
+            System.out.println("Sucesso da pesquisa do: "+ titulo);  }
+        else {
+            System.out.println("Livro nao encontrado na busca: " + titulo);
+        }
+
+        // utilizando outro metodo para exibir os detalhes do livro definido na classe Livro
+        Livro.exibirDetalhes(livro1);
+        Livro.exibirDetalhes(livro2);
+
+    }
+}
---
+++ src/POO/Work1/Livro.java
@@ -0,0 +1,58 @@
+package POO.Work1;
+
+public class Livro {
+
+    private String  titulo;
+    private String  autor;
+    private int  anoPublicacao;
+
+    public Livro(){}
+
+    @Override
+    public String toString() {
+        return "Livro{" +
+                "titulo='" + titulo + '\'' +
+                ", autor='" + autor + '\'' +
+                ", anoPublicacao=" + anoPublicacao +
+                '}';
+    }
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    public String getAutor() {
+        return autor;
+    }
+
+    public void setAutor(String autor) {
+        this.autor = autor;
+    }
+
+    public int getAnoPublicacao() {
+        return anoPublicacao;
+    }
+
+    public void setAnoPublicacao(int anoPublicacao) {
+        if (anoPublicacao <= 0) {
+            System.out.println("O ano de publicação não pode ser negativo.");
+        }
+        this.anoPublicacao = anoPublicacao;
+    }
+
+
+    public static void exibirDetalhes(Livro livro){
+
+        System.out.println();
+        System.out.println("Exibindo detalhes do livro");
+        System.out.println("Titulo:" + livro.getTitulo());
+        System.out.println("Autor:" + livro.getAutor());
+        System.out.println("Ano:" + livro.getAnoPublicacao());
+
+}
+
+}
